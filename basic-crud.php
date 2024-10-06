01b1d725765b46d7e67f0f976c78a246671ae9ac
Tanvir <tanvirlaravel@gmail.com>
2024-10-06 13:09:49 +0600

    initail commit

---
+++ basic-crud.php
@@ -0,0 +1,12 @@
+<?php 
+/*
+* Plugin Name: Basic CRUD
+* Plugin URI:  https://www.basic-crud.com
+* Description: Basic CRUD Operations
+* Version:     1.0
+* Author:      Tanvir
+* Author URI:  https://github.com/tanvirlaravel
+* License:     GPL2
+* License URI: https://www.gnu.org/licenses/gpl-2.0.html
+* Text Domain: basic-crud
+*/
