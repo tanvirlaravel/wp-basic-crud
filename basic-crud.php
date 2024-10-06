1b43720bf76b5a2d7d01043429f8b7b55947bef9
Tanvir <tanvirlaravel@gmail.com>
2024-10-06 13:20:48 +0600

    database table created after the plugin is installed

--- basic-crud.php
+++ basic-crud.php
@@ -10,3 +10,26 @@
 * License URI: https://www.gnu.org/licenses/gpl-2.0.html
 * Text Domain: basic-crud
 */
+
+register_activation_hook(__FILE__, 'crudOperationTable');
+
+function crudOperationTable(){
+    global $wpdb;
+
+    $charset_collate = $wpdb->get_charset_collate();
+    $table_name = $wpdb->prefix . 'userstable';
+
+    $sql = "CREATE TABLE `$table_name` (
+        `user_id` int(11) NOT NULL AUTO_INCREMENT,
+        `name` varchar(220) DEFAULT NULL,
+        `email` varchar(220) DEFAULT NULL,
+        PRIMARY KEY(user_id)
+        ) $charset_collate;
+        ";
+
+    if($wpdb->get_var("SHOW TABLES LIKE '$table_name'") != $table_name){
+        require_once( ABSPATH . 'wp-admin/includes/upgrade.php' );
+        dbDelta($sql);
+    }
+}
+
